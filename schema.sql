f43a2d06f7abb515266e56731e5fa7d0216e6177
gongzhen <gongzhengz@gmail.com>
2014-09-21 15:56:30 -0700

    develop branch

---
+++ schema.sql
@@ -0,0 +1,16 @@
+drop table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+  title text not null,
+  text text not null
+);
+
+drop table if exists users;
+create table users (
+  id integer primary key autoincrement,
+  name text not null,
+  password text not null
+);
+
+INSERT INTO users (name, password)  
+VALUES ("gongzhen", "gongzhen");
